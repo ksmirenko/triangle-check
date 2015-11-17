d8338c9e5b37862a933fe631d1f46c36207c3b78
Kirill Smirenko <cyrussmith1865@gmail.com>
2015-11-17 16:10:22 +0300

    Removed everything related to builder. Only source code remains.

--- test.bat
+++
@@ -1,8 +0,0 @@
-if "%main%"=="" goto :EOF
-echo Testing...
-"%NUNIT_CONSOLE%" "%TEST_LIB%" >> log.txt 2>&1
-if ERRORLEVEL 1 (
-	set error=Test_failure
-    echo ERROR: Testing failed!
-	sendEmail.bat
-)
